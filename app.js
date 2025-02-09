07a60cf9275e7a50697e534ac5f5ff8c9a22b7e2
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-10 01:13:03 +0500

    add

---
+++ app.js
@@ -0,0 +1,31 @@
+// let user =prompt("city name")
+// if(user="karachi"){
+//     document.write(
+//     "Welcome to city of  lights")};
+// let user = prompt("whats your gender");
+// if(user === "male"){
+//     document.write("Good Morning Sir")
+
+// }
+// else{( user === "female")
+//     document.write("Good Morning Maam");
+// }
+// let user = prompt("color of road traffic signal")
+
+// document.write("<table>")
+// document.write("<tr><th>signal color</th><tr><th>message</th></tr>")
+// if (user === "red"){
+// document.write("<tr><td>RED</td> <td>must stop</tr>")}
+// else (user === "yellow");{
+// document.write("<tr><td>Yellow</td> <td>ready to move</tr>")};
+// if (user === "green") {
+//   document.write("<tr><td>green</td> <td>move now</tr>")
+// }
+
+
+
+
+
+// document.write(
+
+// "</table>")
