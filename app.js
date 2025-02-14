e7b81af303d462dffb14739ff43550d63ce5c13c
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-14 23:10:42 +0500

    please add

--- app.js
+++ app.js
@@ -20,15 +20,320 @@
 // document.write("<tr><td>Yellow</td> <td>ready to move</tr>")};
 // if (user === "green") {
 //   document.write("<tr><td>green</td> <td>move now</tr>")
-// }
+// }// document.write(
+
+// "</table>")
 // let user=prompt("please tell me how much litres")
 // if (user <0.25) {alert("please refill the fuel in your car")
     
 // }
 
+// let a = 4
+// if (++a===5) {alert("given condition for variable a is true")
+    
+// }
+// let b =82
+// if (b++===83) {alert ("given condition for variable b is true")
+    
+// }else{alert("not true")}
+// let c = 12
+// if (c++===13) {alert("condition 1 is true"); 
+    
+// }
+// else if (c===13) {alert("condition 2 is true");
+    
+// } 
+// else if (++c <14) {alert("condition 3 is true");}
+// else if(c===14) {alert("condition 4 is true"); }
+// let materialCost = 20000
+// let laborCost = 2000
+// let total = (materialCost+laborCost)
+// alert
+// (total
+// )
+// if (true) {alert("True")
+    
+// }
+
+// else (false)
+// {("false")}
+// if (car<cat) {alert("car is smaller than cat")}
+
+// else{"not"}
+// let subj1 = +prompt("enter your marks1")
+// let subj2 = +prompt("enter your marks2")
+// let subj3 = +prompt("enter your marks3")
+// let totakMarks = (subj1+subj2+subj3
+// )
+// let total = 200
+
+// let percentage = alert(totakMarks/total)*100
+
+// if (percentage>=70) {grade= "A1"
+// remarks= "good"
+// console.log(remarks);
+    
+// }
+// else if(percentage>=60){
+//     grade= "B"
+//     remarks = "need to improve"
+//     console.log(remarks);
+
+// }
+// else{
+//     grade= "F"
+//     remarks= "sorry"
+//     console.log(remarks);
+
+// }
+// document.write('<h3>'+'Marksheet'+'</h3>');
+// document.write('Total marks :'+totalMarks+'<br>');
+// document.write('Percentage :'+percentage+'<br>');
+
+// document.write('Grade :'+grade+'<br>');
+// document.write('Remarks :'+remarks+'<br>');
+// let secretNum = 4
+
+// let user = +prompt()
+// if (user==secretNum){alert ("bingo correct ans"
+// )}
+
+// if (++user==secretNum) {"close enough"
+    
+// }
 
 
+// else{
+//     "sorry wrong"
+// }
+// let num = +prompt("please enter")
+// if(num % 3===0){
+//         alert(num+' is divisible by 3')
+//     }
+//     else{
+//         alert(num+' is not  divisible by 3')
+//     }
 
-// document.write(
 
-// "</table>")
+// let num=+prompt('Enter number to check number is even or odd');
+//  if(num % 2===0){
+//         alert(num+' is even')
+//      }
+//      else{
+//          alert(num+' is odd')
+//      }
+
+
+// let temperature=+prompt('Enter your city temperature');
+// if(temperature>40){
+//     alert('It is too hot outside')
+// }
+// else if(temperature>30){
+//      alert('The Weather today is Normal.')
+// }
+// else if(temperature>20){
+//     alert('Today’s Weather is cool.')
+// }
+// else {
+//     alert('OMG! Today’s weather is so Cool.')
+// }
+// let firstNumber=+prompt('Enter first Number');
+// let secondNumber=+prompt('Enter second Number');
+// let operation=prompt('Choose your operation : \n(+, - , * , % ')
+// if(operation==='+'){
+//     alert(firstNumber+secondNumber)
+// }
+// else if(operation==='-'){
+//     alert(firstNumber-secondNumber)
+// }
+// else if(operation==='*'){
+//     alert(firstNumber*secondNumber)
+// }
+// else if(operation==='/'){
+//     alert(firstNumber/secondNumber)
+// }
+// else{
+//     alert(firstNumber%secondNumber)
+// }
+// let character = prompt('Please enter your character to checks whether the given input is a number, uppercase letter or lower case letter');
+// let value = character.charCodeAt(0);
+// / //Checking for Number
+// // if (value >= 48 && value <= 57) {
+// //     alert(character + " is number");
+// // }
+
+// // //Checking for UpperCase Letters
+// // else if (value >= 65 && value <= 90) {
+// //     alert(character + " is Uppercase Letter");
+// // }
+
+// // //Checking for LowerCase Letters
+// // else if (value >= 97 && value <= 122) {
+// //     alert(character + " is Lowercase Letter");
+// // }
+
+// // else {
+// //     alert("Given input is not a number or letter");
+// // }
+
+// let firstNumber=+prompt('Enter first number');
+// let secondNumber=+prompt('Enter second number');
+// if(firstNumber>secondNumber){
+//     alert(firstNumber+' is largerthan '+ secondNumber);
+// }
+// else if(firstNumber<secondNumber){
+//     alert(secondNumber+' is larger than '+ firstNumber);
+// }
+// else{
+//     alert(firstNumber+' and '+ secondNumber+' both are equal');
+// }
+// let num=+prompt('Please enter number to check whether positive, negative or zero');
+// if(num>0){
+//     alert(num+' is postive')
+// }
+// else if(num<0){
+//     alert(num+' is negative')
+// }
+// else{
+//     alert(num+' is zero')
+// }
+// let character=prompt('Enter character to check vowel or not');
+// if(character==='a'||character==='A'||character==='e'||character==='E'||character==='i'||character==='I'||character==='o'||character==='O'||character==='u'||character==='U'){
+//     alert(character+ ' is vowel')
+// }
+// else{
+//     alert(character+' is not vowel')
+// }
+// let password='hafsatahir1994';
+// let userPassword=prompt('Enter your password');
+// if(userPassword===''){
+//     alert('Please enter your password');
+// }
+// else if(userPassword===password){
+//      alert('Correct! The password you entered matches the original password.')
+// }
+// else{
+//     alert('Incorrect password')
+// }
+
+// let greeting;
+// let hour=13;
+// if(hour<18){
+//     greeting='Good day'
+//     alert(greeting);
+// }
+// else{
+//     greeting='Good evening';
+//     alert(greeting);
+
+// }
+// let time=+prompt('Please enter time in 24 hours \n clockformat  1900 = 7pm.');
+// if(time>=000 && time<1200){
+//     alert('Good Morning')
+// }
+// else if(time>=1200 && time<1700){
+//     alert('Good Afternoon')
+// }
+// else if(time >=1700 && time<2100){
+//     alert('Good Evening')
+// }
+// else if(time >=2100 && time<=2359){
+//     alert('Good Night')
+// }
+// else{
+//     alert('Invalid')
+// }
+// chp14
+// let people=[];
+// let students = new Array();
+
+// let city = ["khi", "islo", "lahore", "Pindi", "Bhawalpur"];
+// document.write("String Array: " + city + "<br/>" + "<br/>" + "<br/>");
+
+// let num = [200, 202, 208, 256, 284];
+// document.write("Number Array: " + num + "<br/>" + "<br/>" + "<br/>");
+// let boolean = [true, false, true, false];
+// document.write("Boolean Array: " + boolean + "<br/>" + "<br/>" + "<br/>");
+// let data = ["abubakar", "tahir", 2024, true];
+// document.write("Mixed Array: " + data + "<br/>" + "<br/>" + "<br/>");
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PHD"];
+// let count = 0;
+// let i = 0;
+
+// document.write("Qualification" + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>");
+// document.write(++count + ") " + qualifications[i++] + "<br/>" + "<br/>" + "<br/>");
+
+// let students=['hafsa','almas','asmat'];
+// let score=[450,400,350];
+// let total=600;
+// percentage1=(score[0]/total)*100;
+// percentage2=(score[1]/total)*100;
+// percentage3=(score[2]/total)*100;
+// document.write('Score of  '+students[0]+' is  '+score[0]+' . Percentage: '+percentage1+' % '+'<br>')
+// document.write('Score of  '+students[1]+' is  '+score[1]+' . Percentage: '+percentage2+' % '+'<br>')
+// document.write('Score of  '+students[2]+' is  '+score[2]+' . Percentage: '+percentage3+' % '+'<br>')
+// let colors = [];
+// let myColor=prompt('Enter your desire color to be added in the beginning of the array');
+// colors.unshift(myColor);
+
+// document.write('Updated array :'+colors+'<br>')
+// let myColorend=prompt('Enter your desire color to be added at the end  of the array');
+// colors.push(myColorend);
+// document.write('Updated array :'+myColorend+'<br>')
+// let addColor1=prompt('Add first color to the beginning of the array');
+// let addColor2=prompt('Add second color to the beginning of the array');
+// colors.unshift(addColor1,addColor2)
+// document.write("Updated array: " + colors + "<br/>");
+// colors.shift();
+// document.write('Updated array :'+ colors+'<br>')
+// colors.pop();
+// document.write('Updated array :'+ colors+'<br>')
+// let index=+prompt('At which index no you want to add color');
+// let colorName=prompt('And which color ?');
+// colors.splice(index,0,colorName);
+// document.write('Updated array :'+ colors+'<br>')
+// 
+
+// let score=[222,202,116,150];
+// document.write('Scores of Students : '+score+'<br>');   
+// document.write('Ordered Scores of Students : '+score.sort()+'<br>');
+// let citynames=['Karachi','Lahore','Multan','Isl','Quetta'];
+// document.write('Cities List :'+citynames+'<br>'+'<br>')
+// let selectedCities=citynames.slice(2,5);
+// document.write('Selectedcities List :'+selectedCities+'<br>')
+// let array=['This','is','my','cat'];
+// document.write('Array : '+'<br>'+array+'<br>');
+// let string=array.join(' ');
+// document.write('String : '+'<br>'+string);
+
+// let devices = ['keyboard', 'mouse', 'printer', 'monitor'];
+// let out;
+// document.write('Devices :' +devices+ '<br>');
+// out = devices.shift();
+// document.write('Out' + '<br>' + out + '<br>')
+
+// out = devices.shift();
+// document.write('Out' + '<br>' + out + '<br>')
+// out = devices.shift();
+// document.write('Out' + '<br>' + out + '<br>')
+// out = devices.shift();
+// document.write('Out' + '<br>' + out + '<br>')
+// let phoneMenu = ['Apple', 'Samsung', 'Motorola', 'Nokia', 'Sony', 'Haier'];
+// let index = 0;
+// document.write('<select>')
+// document.write('<option>' + '<option>')
+
+// document.write('<option>' + phoneMenu[index++] + '<option>')
+// document.write('<option>' + phoneMenu[index++] + '<option>')
+// document.write('<option>' + phoneMenu[index++] + '<option>')
+// document.write('<option>' + phoneMenu[index++] + '<option>')
+// document.write('<option>' + phoneMenu[index++] + '<option>')
+// document.write('<option>' + phoneMenu[index++] + '<option>')
+// document.write('<select>')
